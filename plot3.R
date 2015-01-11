25779827515e21d7d69861eb49b7bfe8566e2171
MaheshGodi <Mahesh.Godi@gmail.com>
2015-01-11 11:45:18 -0800

    added plot3.r plot3.png

--- plot3.R
+++ plot3.R
@@ -1,48 +1,40 @@
-## Reading data
-rm(Electric_power)
-rm (Electric_power_data )
-Electric_power <- read.table("household_power_consumption.txt",   stringsAsFactors = FALSE,
-                            header = TRUE, comment.char="", quote='\"',
-                            sep = ';', na.strings = "?", check.names = FALSE)
-## Subseting only "2007-02-01" and "2007-02-02"
-Electric_power_data <- subset(Electric_power, subset = (as.Date(Electric_power$Date , "%m/%d/%Y") >= "2007-02-01" & (as.Date(Electric_power$Date , "%m/%d/%Y") <= "2007-02-02") ))
 
-## Creating Date_Time data
-## Date_Time <- paste(as.Date(Electric_power_data$Date), Electric_power_data$Time)
-## Assinging Date_Time data as a new column in Electric_power_data
+library(data.table)
+Electric_power_data <- fread("household_power_consumption.txt")
 
-Electric_power_data$Datetime <- strptime(paste(Electric_power_data$Date, Electric_power_data$Time), "%d/%m/%Y %H:%M:%S")
-head(Electric_power_data)
+## Data Clean 
 
-for(i in c(3:9)) {Electric_power_data[,i] <- as.numeric(as.character(Electric_power_data[,i]))}
+# Change the format of Date  variable
+Electric_power_data$Date <- as.Date(Electric_power_data$Date, format="%d/%m/%Y")
 
-## Histogram genration for plot 1
+# Subset the data  for "2007-02-01" and "2007-02-02"
+Electric_power_data_subset <- Electric_power_data[Electric_power_data$Date=="2007-02-01" | Electric_power_data$Date=="2007-02-02"]
 
-plot(Electric_power_data$Datetime, Electric_power_data$Global_active_power  , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+# Convert data subset to a data frame
 
-png(filename = "plot2.png", width = 480, height = 480, units = "px", bg = "white")
+Electric_power_data_subset <- data.frame(Electric_power_data_subset)
 
-par(mar = c(6, 6, 5, 4))
+# Convert columns to numeric
+for(i in c(3:9)) {Electric_power_data_subset[,i] <- as.numeric(as.character(Electric_power_data_subset[,i]))}
 
+# Create Date_Time variable
+Electric_power_data_subset$Date_Time <- paste(Electric_power_data_subset$Date, Electric_power_data_subset$Time)
 
-plot(data$Datetime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-points(data$Datetime, data$Sub_metering_2, type = "l", xlab = "", ylab = "Energy sub metering", 
-       col = "red")
-points(data$Datetime, data$Sub_metering_3, type = "l", xlab = "", ylab = "Energy sub metering", 
-       col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", 
-                                                                        "Sub_metering_2", "Sub_metering_3"))
+# Convert Date_Time variable to proper format
+Electric_power_data_subset$Date_Time <- strptime(Electric_power_data_subset$Date_Time, format="%Y-%m-%d %H:%M:%S")
 
-plot(Electric_power_data$Datetime, Electric_power_data$Global_active_power, xaxt=NULL, xlab = "", ylab = "Global Active Power (kilowatts)", type="n")
-lines(Electric_power_data$Datetime, Electric_power_data$Global_active_power, type="S")
-hist(Electric_power_data$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", 
-     col = "Red",
-     main = "Global Active Power"  ,
-     ylim = c(0,1200),
-      xlim = c(0,6),
-     xaxp = c(0, 6, 3),
-     breaks = 13)
-## creating plot1.png with dev.copy function
-dev.copy(png, file = "plot1.png", height = 480, width = 480)
+# plot3
+png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "white")
+par(mar = c(7, 6, 5, 4))
+
+plot(Electric_power_data_subset$Date_Time, Electric_power_data_subset$Sub_metering_1, xaxt=NULL, xlab = "", ylab = "Energy sub metering", type="n")
+## Sets up the plot, but does not populate with any data
+lines(Electric_power_data_subset$Date_Time, Electric_power_data_subset$Sub_metering_1, col = "black", type = "S")
+## Plots lines for sub_metering_1
+lines(Electric_power_data_subset$Date_Time, Electric_power_data_subset$Sub_metering_2, col = "red", type = "S")
+## Plots lines for sub_metering_2
+lines(Electric_power_data_subset$Date_Time, Electric_power_data_subset$Sub_metering_3, col = "blue", type = "S")
+## Plots lines for sub_metering_3
+legend("topright", lty = c(1, 1), lwd = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.off()
-
